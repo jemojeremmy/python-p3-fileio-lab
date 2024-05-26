952ce2e5c2b60853b71e0b84f0675f32aeca936b
jemojeremmy2@gmail.com <jemojeremmy2>
2024-05-26 19:32:58 +0300

    new

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
-def write_file(file_name, file_content):
-    pass
+# lib/file_io.py
 
-def append_file(file_name, append_content):
-    pass
+def write_file(file_name, file_content):
+    # Convert file_name to string and add .txt extension
+    file_name_with_extension = str(file_name) + '.txt'
+    # Write the content to the file
+    with open(file_name_with_extension, 'w') as file:
+        file.write(file_content)
+
+def append_file(file_name, file_content):
+    # Convert file_name to string and add .txt extension
+    file_name_with_extension = str(file_name) + '.txt'
+    # Append the content to the file
+    with open(file_name_with_extension, 'a') as file:
+        file.write(file_content)
 
 def read_file(file_name):
-    pass
+    # Convert file_name to string and add .txt extension
+    file_name_with_extension = str(file_name) + '.txt'
+    # Read and return the content of the file
+    with open(file_name_with_extension, 'r') as file:
+        return file.read()
